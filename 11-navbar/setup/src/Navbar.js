3b25285a223912a3be9c9c4ca01892a5c838d1eb
Dino14 <ayushbinjola14@gmail.com>
2023-07-08 21:40:49 +0530

    partially done with 11, amazing css to learn

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,41 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [showLinks, setShowLinks] = useState(false);
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button
+            // className="nav-toggle"
+            onClick={() => {
+              setShowLinks(!showLinks);
+            }}
+          >
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className="links-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
